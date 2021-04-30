b8b3caf7cbd68b2e8179d49a8c2bee26eb7cc862
arthur-er <arthur-er@protonmail.me>
2021-04-30 07:19:34 -0300

    refactor(users): include password in UserEntity

--- app/modules/users/domain/userEntity.ts
+++ app/modules/users/domain/userEntity.ts
@@ -1,12 +1,12 @@
 import { Id } from "app/core/domain/valueObjects/id"
 import { MembershipEntity } from "app/modules/membership/domain/membershipEntity"
-import { Membership } from "db"
 import { Email } from "./valueObjects/userEmail"
 
 export interface IUserEntity {
   id: Id
   name: string
   email: Email
+  hashedPassword: string
   memberships?: MembershipEntity[]
 }
 
@@ -15,10 +15,12 @@ export class UserEntity implements IUserEntity {
   name: string
   email: Email
   memberships?: MembershipEntity[]
+  hashedPassword: string
 
   constructor(props: IUserEntity) {
     this.id = new Id(props.id.value)
     this.name = props.name
+    this.hashedPassword = props.hashedPassword
     this.email = new Email(props.email.value)
     this.memberships = props.memberships ? props.memberships : undefined
     Object.freeze(this)
