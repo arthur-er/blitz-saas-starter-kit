6850c2c73a4b1999647265a00844662e82d78ee2
arthur-er <arthur-er@protonmail.me>
2021-04-30 05:39:15 -0300

    feat(users): create user mutation

---
+++ app/modules/users/mutations/createUser.ts
@@ -0,0 +1,28 @@
+import { resolver } from "blitz"
+import db from "db"
+import { Role } from "types"
+import * as z from "zod"
+import { Email } from "../domain/valueObjects/userEmail"
+import { usersRepository } from "../infra/usersRepository"
+
+const password = z.string().min(10).max(100)
+
+const CreateUserSchema = z.object({
+  email: z.string().email(),
+  name: z.string(),
+  password,
+  orgName: z.string().min(3).max(30),
+})
+
+export default resolver.pipe(resolver.zod(CreateUserSchema), async (input) => {
+  const email = new Email(input.email)
+  const user = await usersRepository.createUser({ ...input, email })
+
+  /* await ctx.session.$create({
+    userId: user.id.value,
+    orgId: user.memberships![0].organizationId.value,
+    roles: [user.memberships![0].role.value!, user.memberships![0].organization.role.value!],
+  }) */
+
+  return user
+})
