feb33eda8e460855e3fd3e92ee506452fcabceb5
Arthur Emanuel <arthur-er@pm.me>
2021-05-01 05:40:03 -0300

    Scaffold generation (#3)
    * feat(db): add webhooks migration
    
    * feat: add contracts to register app types
    
    * feat: event emitter for app
    
    Event listeners should be placed inside modules
    ```typescript
    // {{module}}/listeners/CreateModuleListener.ts
    import { Events } from 'app/start/events'
    
    export default listener = () => {}
    
    // app/start/events.ts
    Events.on('event', listener)
    ```
    
    * feat(webhooks): webhooks domain
    
    * feat(events): register webhook events
    
    * fix(webhooks): fix typings on createWebhook mutation
    
    * feat: mark webhook calling done on README
    
    * fix(events): remove url from hookable event data and use it from the webhook instead
    
    * feat: code scaffolding with plop
    
    Co-authored-by: arthur-er <arthur-er@protonmail.me>

---
+++ tools/plopfile.js
@@ -0,0 +1,53 @@
+const pluralize = require("pluralize")
+module.exports = function (plop) {
+  plop.setHelper("plural", (term) => {
+    return pluralize.plural(term)
+  })
+  plop.setGenerator("App Module", {
+    description: "generate a app module with domain, repository, queries and mutations",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "module name? (singular please)",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "../app/modules/{{plural name}}/domain/{{name}}Entity.ts",
+        templateFile: "./templates/domain/entity.hbs",
+      },
+      {
+        type: "add",
+        path: "../app/modules/{{plural name}}/domain/{{name}}Factory.ts",
+        templateFile: "./templates/domain/factory.hbs",
+      },
+      {
+        type: "add",
+        path: "../app/modules/{{plural name}}/infra/{{plural name}}Repository.ts",
+        templateFile: "./templates/infra/repository.hbs",
+      },
+      {
+        type: "add",
+        path: "../app/modules/{{plural name}}/mutations/create{{sentenceCase name}}.ts",
+        templateFile: "./templates/mutations/create.hbs",
+      },
+      {
+        type: "add",
+        path: "../app/modules/{{plural name}}/mutations/update{{sentenceCase name}}.ts",
+        templateFile: "./templates/mutations/update.hbs",
+      },
+      {
+        type: "add",
+        path: "../app/modules/{{plural name}}/queries/get{{sentenceCase (plural name)}}.ts",
+        templateFile: "./templates/queries/getMany.hbs",
+      },
+      {
+        type: "add",
+        path: "../app/modules/{{plural name}}/queries/find{{sentenceCase name}}ById.ts",
+        templateFile: "./templates/queries/getOne.hbs",
+      },
+    ],
+  })
+}
