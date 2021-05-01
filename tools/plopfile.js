4bf75849967b1bc498b52da4ad9c85d6e38161fa
arthur-er <arthur-er@protonmail.me>
2021-05-01 06:38:38 -0300

    fix: skip generation if files exist on plopfile

--- tools/plopfile.js
+++ tools/plopfile.js
@@ -17,36 +17,43 @@ module.exports = function (plop) {
         type: "add",
         path: "../app/modules/{{plural name}}/domain/{{name}}Entity.ts",
         templateFile: "./templates/domain/entity.hbs",
+        skipIfExists: true,
       },
       {
         type: "add",
         path: "../app/modules/{{plural name}}/domain/{{name}}Factory.ts",
         templateFile: "./templates/domain/factory.hbs",
+        skipIfExists: true,
       },
       {
         type: "add",
         path: "../app/modules/{{plural name}}/infra/{{plural name}}Repository.ts",
         templateFile: "./templates/infra/repository.hbs",
+        skipIfExists: true,
       },
       {
         type: "add",
         path: "../app/modules/{{plural name}}/mutations/create{{sentenceCase name}}.ts",
         templateFile: "./templates/mutations/create.hbs",
+        skipIfExists: true,
       },
       {
         type: "add",
         path: "../app/modules/{{plural name}}/mutations/update{{sentenceCase name}}.ts",
         templateFile: "./templates/mutations/update.hbs",
+        skipIfExists: true,
       },
       {
         type: "add",
         path: "../app/modules/{{plural name}}/queries/get{{sentenceCase (plural name)}}.ts",
         templateFile: "./templates/queries/getMany.hbs",
+        skipIfExists: true,
       },
       {
         type: "add",
         path: "../app/modules/{{plural name}}/queries/find{{sentenceCase name}}ById.ts",
         templateFile: "./templates/queries/getOne.hbs",
+        skipIfExists: true,
       },
     ],
   })
